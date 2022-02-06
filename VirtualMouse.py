b68a9bc0d451d473fc6c025510aae92749e19c96
Md Owais Umar <owaisumar9@gmail.com>
2022-02-06 19:02:55 +0530

    Added function for right click

--- VirtualMouse.py
+++ VirtualMouse.py
@@ -3,9 +3,11 @@ from cv2 import cv2
 import HandTrackingModule as htm
 import mediapipe as mp
 import time
-import autopy
+from autopy import mouse, screen
+import pyautogui
+from cvzone import cornerRect
 
-wScr, hScr = autopy.screen.size()
+wScr, hScr = screen.size()
 tRange = 75  # Touching range
 smoothening = 5
 prevLocX, prevLocY = 0, 0
@@ -13,17 +15,17 @@ prevLocX, prevLocY = 0, 0
 cap = cv2.VideoCapture(0)
 cap.set(3, 1230)
 cap.set(4, 600)
-cTime = 0
-pTime = 0
+mouseClicked = False
 detector = htm.HandDetector(maxHands=1)
 
 while True:
     # 1. Find hand landmarks
     success, img = cap.read()
     hCam, wCam = img.shape[0:2]
-    img = detector.findHands(img)
+    img = detector.findHands(img, draw=False)
     LMList, bbox = detector.findPositions(img, draw=False)
-    cv2.rectangle(img, (tRange, tRange), (wCam - tRange, hCam - tRange), (0, 255, 0), thickness=2)
+    # cv2.rectangle(img, (tRange, tRange), (wCam - tRange, hCam - tRange), (0, 255, 0), thickness=2)
+    cornerRect(img, (tRange, tRange, wCam - 2*tRange, hCam - 2*tRange))
 
     # 2. Get the tip of index and middle fingers
     if len(LMList):
@@ -38,16 +40,17 @@ while True:
         # 4. Convert Coordinates
         x3 = np.interp(x1, (tRange, wCam - tRange), (0, wScr))
         y3 = np.interp(y1, (tRange, hCam - tRange), (0, hScr))
-        cv2.circle(img, (x1, y1), 10, (255, 0, 255), cv2.FILLED)
+        cv2.circle(img, (x1, y1), 5, (255, 0, 255), cv2.FILLED)
 
         # 5. Smoothen Values
         currLocX = prevLocX + (x3 - prevLocX) / smoothening
         currLocY = prevLocY + (y3 - prevLocY) / smoothening
+        # currLocX, currLocY = x3, y3
 
         # 6. Only index fingers up: Moving mode
         if fingers[1] and not fingers[2]:
             # 7. Move mouse
-            autopy.mouse.move(currLocX, currLocY)
+            mouse.move(currLocX, currLocY)
             prevLocX, prevLocY = currLocX, currLocY
 
         # 8. Both index and middle fingers up: Clicking mode
@@ -55,24 +58,15 @@ while True:
             # 9. Find distance between fingers
             l, _ = detector.getDistance(img, 8, 12, draw=False)
             # print(l)
-            if l < 30:
+            if l < 50:
                 cv2.circle(img, (x1, y1), 10, (0, 255, 0), cv2.FILLED)
                 # 10. Click mouse if distance is short
-                autopy.mouse.click()
+                pyautogui.click()
 
-        # 11. Click and drag when index, middle and ring fingers are all up
-        # if fingers[1] and fingers[2] and fingers[3]:
-        #     l1, _ = detector.getDistance(img, 8, 12, draw=False)
-        #     l2, _ = detector.getDistance(img, 12, 16, draw=False)
-        #     print(l1, l2)
-        #     if l1 < 30 and l2 < 35:
-        #         cv2.circle(img, (x1, y1), 10, (0, 255, 0), cv2.FILLED)
-        #         # 12. Click and drag mouse if distance is short
-        #         autopy.mouse.toggle('LEFT', True)
-        #         autopy.mouse.move(currLocX, currLocY)
-        #         prevLocX, prevLocY = currLocX, currLocY
+        # 11. Right click when thumb is up
+        if fingers[0] and fingers[1]:
+            pyautogui.click(button='right')
 
-    # 12. Frame rate
     detector.showFPS(img, (30, 30), box=True)
     cv2.imshow("Webcam", img)
     cv2.waitKey(1)
